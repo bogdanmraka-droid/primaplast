edc507dbbcf9a4deee38bbba171e2179aa5535f6
bogdanmraka-droid <bogdan.mraka@gmail.com>
2026-02-21 17:27:46 +0000

    New version2

--- script.js
+++ script.js
@@ -10,4 +10,25 @@ const onProgress = (event) => {
     progressBar.classList.remove('hide');
   }
 };
+                  const modelViewerVariants = document.querySelector("model-viewer#shoe");
+                  const select = document.querySelector('#variant');
+
+                  modelViewerVariants.addEventListener('load', () => {
+                    const names = modelViewerVariants.availableVariants;
+                    for (const name of names) {
+                      const option = document.createElement('option');
+                      option.value = name;
+                      option.textContent = name;
+                      select.appendChild(option);
+                    }
+                    // Adds a default option.
+                    const option = document.createElement('option');
+                    option.value = 'default';
+                    option.textContent = 'Default';
+                    select.appendChild(option);
+                  });
+
+                  select.addEventListener('input', (event) => {
+                    modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
+                  });
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
