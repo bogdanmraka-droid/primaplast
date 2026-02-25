4adb5ec15d2aeef1139c51436560cf5b45c13648
bogdanmraka-droid <bogdan.mraka@gmail.com>
2026-02-25 10:54:30 +0000

    Hernia4

--- script.js
+++ script.js
@@ -2,21 +2,6 @@ const iframe = document.getElementById('api-frame');
 const uid = 'c408470f192341d883fbbbcdac73aff4';
 const client = new Sketchfab(iframe);
 
-function getBase64Texture(url, callback) {
-    const img = new Image();
-    img.crossOrigin = 'anonymous';
-    img.onload = function() {
-        const canvas = document.createElement('canvas');
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-        callback(canvas.toDataURL('image/jpeg'));
-    };
-    img.src = url;
-}
-
-
 client.init(uid, {
     success: function onSuccess(api) {
         api.start();
